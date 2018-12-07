b40e4778e8ab0894512a17a1f3b5c9c6639a030e
Scott Grant <scott.grant@thrasher.io>
2018-12-07 14:26:34 +1100

    Adds tests to all exchanges to test wrapper function CancelAllOrders

--- exchanges/zb/zb_test.go
+++ exchanges/zb/zb_test.go
@@ -300,3 +300,31 @@ func TestCancelExchangeOrder(t *testing.T) {
 		t.Errorf("Could not cancel order: %s", err)
 	}
 }
+
+func TestCancelAllExchangeOrders(t *testing.T) {
+	// Arrange
+	z.SetDefaults()
+	TestSetup(t)
+
+	if !isRealOrderTestEnabled() {
+		t.Skip()
+	}
+
+	z.Verbose = true
+	currencyPair := pair.NewCurrencyPair(symbol.LTC, symbol.BTC)
+
+	var orderCancellation = exchange.OrderCancellation{
+		OrderID:       "1",
+		WalletAddress: "1F5zVDgNjorJ51oGebSvNCrSAHpwGkUdDB",
+		AccountID:     "1",
+		CurrencyPair:  currencyPair,
+	}
+
+	// Act
+	err := z.CancelAllOrders(orderCancellation)
+
+	// Assert
+	if err != nil {
+		t.Errorf("Could not cancel order: %s", err)
+	}
+}
